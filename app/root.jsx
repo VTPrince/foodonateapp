ad973bc6dadf6dc734be88d218dba3bff5696e54
Varan Gulati <varangulati012@gmail.com>
2023-06-24 19:00:38 +0530

    added css link

--- app/root.jsx
+++ app/root.jsx
@@ -1,31 +1,19 @@
-import { cssBundleHref } from "@remix-run/css-bundle";
-
-import {
-  Links,
-  LiveReload,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
-} from "@remix-run/react";
-
-export const links = () => [
-  ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
-];
+import { Links,LiveReload, Outlet } from "@remix-run/react";
 
 export default function App() {
   return (
     <html lang="en">
       <head>
         <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width,initial-scale=1" />
-        <Meta />
+        <meta
+          name="viewport"
+          content="width=device-width,initial-scale=1"
+        />
+        <title>Remix: So great, it's funny!</title>
         <Links />
       </head>
       <body>
         <Outlet />
-        <ScrollRestoration />
-        <Scripts />
         <LiveReload />
       </body>
     </html>
