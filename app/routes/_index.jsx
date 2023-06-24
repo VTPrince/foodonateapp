8be6674e4597673b7d66cc02a2bc079ac5e0c3e3
Varan Gulati <varangulati012@gmail.com>
2023-06-24 19:01:11 +0530

    added css link

--- app/routes/_index.jsx
+++ app/routes/_index.jsx
@@ -1,39 +1,10 @@
-export const meta = () => {
-  return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
-  ];
-};
+import stylesUrl from "~/styles/index.css";
+import { Link } from "@remix-run/react";
+export const links = () => [
+  { rel: "stylesheet", href: stylesUrl },
+];
 
-export default function Index() {
-  return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-    </div>
-  );
+
+export default function IndexRoute(){
+    return <div><Link to="jokes">Read Jokes</Link></div>
 }
